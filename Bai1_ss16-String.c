cc62cef556ac550bc7078a8875020e8864e2a3b3
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-12-12 07:20:53 +0700

    Bai1_ss16-String.c

--- Bai1_ss16-String.c
+++ Bai1_ss16-String.c
@@ -3,7 +3,9 @@ int main(){
 	int string[100];
 	printf("Vui long nhap vao chuoi bat ki: ");
 	gets(string);
+	int size = strlen(string);
 	
 	printf("Chuoi vua nhap la: %s", string);
+	printf("\nCo do dai la: %d",size);
 	return 0;
 }
